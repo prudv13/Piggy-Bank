a856d792b33fb7fee8e4e8e28d3ef74ff2cbd270
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-09-20 11:16:39 +0530

    connected redux store to react app with provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import store from './store/store'
+import { Provider } from 'react-redux'
 
-store.dispatch({type: 'account/deposit', payload: 250})
-console.log(store.getState());
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 )
